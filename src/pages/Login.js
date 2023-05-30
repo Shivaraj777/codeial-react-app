f5f43d87aa4acde89b56902ee5c61133b8ac89f2
Shivaraj M Shetty <shivaraj916462@gmail.com>
2023-05-30 23:44:49 +0530

    Creating UI for LogIn page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import styles from '../styles/login.module.css';
 
 function Login() {
   return (
-    <div>
-        Login
-    </div>
-  )
+    <form className={styles.loginForm}>
+      <span className={styles.loginSignupHeader}>Log In</span>
+      <div className={styles.field}>
+         <input type='email' name='email' placeholder='Enter email' required />
+      </div>
+      <div className={styles.field}>
+        <input type='password' name='password' placeholder='Enter password' required />
+      </div>
+      <div className={styles.field}>
+        <button>LogIn</button>
+      </div>
+    </form>
+  );
 }
 
 export default Login;
