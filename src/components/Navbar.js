831ff2958d33461326e360dea610234bfef08142
Shivaraj M Shetty <shivaraj916462@gmail.com>
2023-06-03 18:19:20 +0530

    changing Navbar base of user authentication

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,35 +2,41 @@ import React from 'react'
 import styles from '../styles/navbar.module.css';
 import UserPicture from '../images/man.png';
 import {Link} from 'react-router-dom';
+import { useAuth } from '../hooks';
 
 function Navbar() {
+  const auth = useAuth();
+
   return (
     <div className={styles.nav}>
-      {/* left section of Navbar */}
       <div className={styles.leftNav}>
         <Link to='/'>
           <img alt='app-logo' src='https://ninjasfiles.s3.amazonaws.com/0000000000003454.png' />
         </Link>
       </div>
-      {/* right section of nav bar */}
       <div className={styles.rightNav}>
-        <div className={styles.user}>
+        { auth.user && <div className={styles.user}>    {/* if user is logged in show the user details in navbar */}
           <a href='/'>
             <img src={UserPicture} alt='user-dp' className={styles.userDp}/>
           </a>
-          <span>Shivaraj</span>
-        </div>
+          <span>{auth.user.name}</span>
+        </div> }
         <div className={styles.navLinks}>
           <ul>
-            <li>
-              <Link to='/login'>Login</Link>
-            </li>
-            <li>
-              <Link to='/'>Logout</Link>
-            </li>
-            <li>
-              <Link to='/'>Register</Link>
-            </li>
+            {auth.user ? ( 
+              <>
+                <li onClick={auth.logout}>
+                  <Link to='/'>Logout</Link>
+                </li>
+              </>) : (
+              <>
+                <li>
+                  <Link to='/login'>Login</Link>
+                </li>
+                <li>
+                  <Link to='/'>Register</Link>
+                </li>
+              </>)}
           </ul>
         </div>
       </div>
